410ebfe820f1e9dd0c899752668e483c814b4dbd
Azizcan Cetin <Azizcancetin444@email.com>
2022-10-09 10:45:24 +0300

    first commit

---
+++ Lesson2/src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    //camel casing
+    public static void main(String[] args) {
+        //case sensitive (Küçük büyük harf duyarlı)
+
+        int ogrenciSayisi = 12;
+        String OgrenciSayim = "Öğrenci Sayım ";
+        System.out.println(OgrenciSayim + ogrenciSayisi);
+        System.out.println(OgrenciSayim + ogrenciSayisi);
+        System.out.println(OgrenciSayim + ogrenciSayisi);
+        System.out.println(OgrenciSayim + ogrenciSayisi);
+        System.out.println(OgrenciSayim + ogrenciSayisi);
+
+        //reusability (Tekrar kullanılabilir)
+
+        //Bir kere tanımlayıp her yerde kullabilmek ve veriyi değiştirmemiz gerektiğinde bir kere değiştirip sorunu hallederiz
+    }
+}
